aa9f10b912c7106f7e118753d4c6e3e5ff67c4c8
Amber Farrington <amberf0712@outlook.com>
2016-10-14 09:57:23 -0700

    Add functionality to lookup Github userby username

--- js/project-interface.js
+++ js/project-interface.js
@@ -1,49 +1,18 @@
-var MovieFinder = require('./../js/project.js').movieFinderModule;
-
-function displayUser(user){
-  if (typeof user === "string") {
-    $("#data").text(movies);
-  }else {
-    var userDomElements = "";
-    for (var i = 0; i < user.length; i++) {
-      userDomElements += "<tr>";
-      userDomElements += "<td>" + (i+1) + "</td>";
-      userDomElements += "<td>" + muser[i].show_title + "</td>";
-      userDomElements += "<td>" + movies[i].release_year + "</td>";
-      if (muser[i].runtime === "N/A") {
-        userDomElements += "<td>Data not Available</td>";
-      }else{
-        userDomElements += "<td>" + user[i].runtime + "</td>";
-      }
-      if(user[i].director === ""){
-        userDomElements += "<td>Data not Available</td>";
-      }else{
-          userDomElements += "<td>" + user[i].director + "</td>";
-      }
-      userDomElements += "<td>&nbsp;<img onerror = ' this.onerror = null; this.src = \" /img/not-found.png \" ; ' class = 'poster' src='" + movies[i].poster + "'></td>";
-      userDomElements += "</tr>";
-    }
-    $("#data").append("<table class='table'>" +
-                        "<thead>"+
-                          "<tr>"+
-                          "<th>#</th>"+
-                            "<th>Username</th>" +
-                            "<th>Number of Repos</th>" +
-                            "<th>Github Link</th>" +
-                          "</tr>" +
-                          "</thead>"+
-                          userDomElements +
-                          "</tbody></table>"
-                      );
+var GithubLookup = require('./../js/project.js').githubLookupModule;
+function displayRepos(repos){
+  console.log(repos);
+  var repoList = ""; //repo list empty array
+  for (var i = 0; i < repos.length; i++) { // start at index 0 and check length of user info and append into empty <ul>
+    repoList += "<li><br><b>Repository: " + repos[i].name + "</b></br></li>";
+    repoList += "<li>Description: " + repos[i].description + "</li>";
   }
+  $("#data").append("<ul>"+ repoList + "</ul>");
 }
-
 $(document).ready(function() {
-  $("#getUserInfo").submit(function(event){
+  $("#getUserInfo").submit(function(event){ //form submit to look up user/repo info
     event.preventDefault();
-    $('#data').text("");
-    var userInfo = $("#userName").val();
-    var userFinder = new UserFinder(userInfo);
-    userFinder.getRepos(displayUser);
+    var username = $("#userName").val();
+    var githublookup = new GithubLookup();
+    githublookup.getRepos(username, displayRepos);
   });
 });
--- js/project.js
+++ js/project.js
@@ -1,26 +1,15 @@
-UserFinder = function(first, last){
-  this.first = first;
-  this.last = last;
-}
+var apiKey = require('./../.env').apiKey;
 
-function compare(a,b) {
-  if (a.show_title < b.show_title)
-    return -1;
-  if (a.show_title > b.show_title)
-    return 1;
-  return 0;
-}
+GithubLookup = function(){
 
-UserFinder.prototype.getUserInfo = function(displayUser){
-  exports.getRepos = function(){
-    $.get('https://api.github.com/users/daneden?access_token=' + ' 9d77a6affb3c817e524b4992f5b37e439f328a88').then(function(response){
-      console.log(response);
-    }).fail(function(error){
-      console.log(error.responseJSON.message);
-    });
-  };
+};
+//Github API protoype to access user info
+GithubLookup.prototype.getRepos = function(username, displayRepos){
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
+    displayRepos(response);
+  }).fail(function(error){
+    displayRepos(error.responseJSON.message);
+  });
+};
 
-
-exports.userFinderModule = UserFinder;
-
-//this is the code to make the API call from github
+exports.githubLookupModule = GithubLookup;
