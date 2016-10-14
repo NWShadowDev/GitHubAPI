3b639af9184ab4e1378641760caaf4f3e7e74778
Amber Farrington <amberf0712@outlook.com>
2016-10-14 10:55:13 -0700

    Changed display layout of repo data into a table

--- js/project-interface.js
+++ js/project-interface.js
@@ -3,12 +3,27 @@ function displayRepos(repos){
   console.log(repos);
   var repoList = ""; //repo list empty array
   for (var i = 0; i < repos.length; i++) { // start at index 0 and check length of user info and append into empty <ul>
-    repoList += "<li><br><b>Repository: " + repos[i].name + "</b></br></li>";
-    repoList += "<li>Description: " + repos[i].description + "</li>";
+    repoList += "<tr>";
+          repoList += "<td>" + (i+1) + "</td>";
+          repoList += "<td>" + repos[i].name + "</td>";
+          repoList += "<td>" + repos[i].language + "</td>";
+          repoList += "<td>" + repos[i].html_url + "</td>";
+          repoList += "<td>" + repos[i].description + "</td>";
   }
-  $("#data").append("<ul>"+ repoList + "</ul>");
-
-}
+  $("#data").append("<table class='table'>" +
+                         "<thead>"+
+                           "<tr>"+
+                           "<th>#</th>"+
+                             "<th>Repository</th>" +
+                             "<th>Language</th>" +
+                             "<th>URL</th>" +
+                             "<th>Description</th>" +
+                           "</tr>" +
+                           "</thead>"+
+                           repoList +
+                           "</tbody></table>"
+                       );
+  }
 
 $(document).ready(function() {
   $("#getUserInfo").submit(function(event){ //form submit to look up user/repo info
